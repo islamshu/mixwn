84bf9e2306203cb15d7ceb05fadd04969c0faae9
islamshu12 <islamshu12@gmail.com>
2022-05-01 16:39:05 +0300

    first commit

---
+++ resources/views/backend/setup_configurations/activation.blade.php
@@ -0,0 +1,232 @@
+@extends('backend.layouts.app')
+
+@section('content')
+
+<h4 class="text-center text-muted">{{translate('System')}}</h4>
+<div class="row">
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+            	<h5 class="mb-0 h6 text-center">{{translate('HTTPS Activation')}}</h5>
+            </div>
+            <div class="card-body text-center">
+                <label class="aiz-switch aiz-switch-success mb-0">
+                    <input type="checkbox" onchange="updateSettings(this, 'FORCE_HTTPS')" <?php if(env('FORCE_HTTPS') == 'On') echo "checked";?>>
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Maintenance Mode Activation')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <label class="aiz-switch aiz-switch-success mb-0">
+                    <input type="checkbox" onchange="updateSettings(this, 'maintenance_mode')" <?php if(\App\BusinessSetting::where('type', 'maintenance_mode')->first()->value == 1) echo "checked";?>>
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Disable image optimization?')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <label class="aiz-switch aiz-switch-success mb-0">
+                    <input type="checkbox" onchange="updateSettings(this, 'disable_image_optimization')" <?php if(get_setting('disable_image_optimization') == 1) echo "checked";?>>
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<h4 class="text-center text-muted mt-4">{{translate('Business Related')}}</h4>
+<div class="row">
+  
+  
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Conversation Activation')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <label class="aiz-switch aiz-switch-success mb-0">
+                    <input type="checkbox" onchange="updateSettings(this, 'conversation_system')" <?php if(\App\BusinessSetting::where('type', 'conversation_system')->first()->value == 1) echo "checked";?>>
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>
+    </div>
+   
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Category-based Commission')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <label class="aiz-switch aiz-switch-success mb-0">
+                    <input type="checkbox" onchange="updateSettings(this, 'category_wise_commission')" <?php if(\App\BusinessSetting::where('type', 'category_wise_commission')->first()->value == 1) echo "checked";?>>
+                    <span class="slider round"></span>
+                </label>
+                <div class="alert" style="color: #004085;background-color: #cce5ff;border-color: #b8daff;margin-bottom:0;margin-top:10px;">
+                    {{ translate('After activate this option Seller commision will be disabled and You need to set commission on each category otherwise Admin will not get any commision')}}. <a href="{{ route('categories.index') }}">{{ translate('Set Commisssion Now')}}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Email Verification')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <label class="aiz-switch aiz-switch-success mb-0">
+                    <input type="checkbox" onchange="updateSettings(this, 'email_verification')" <?php if(\App\BusinessSetting::where('type', 'email_verification')->first()->value == 1) echo "checked";?>>
+                    <span class="slider round"></span>
+                </label>
+                <div class="alert" style="color: #004085;background-color: #cce5ff;border-color: #b8daff;margin-bottom:0;margin-top:10px;">
+                    You need to configure SMTP correctly to enable this feature. <a href="{{ route('smtp_settings.index') }}">Configure Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<h4 class="text-center text-muted mt-4">{{translate('Payment Related')}}</h4>
+<div class="row">
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header text-center bord-btm">
+                <h3 class="mb-0 h6 text-center">{{translate('Paypal Payment Activation')}}</h3>
+            </div>
+            <div class="card-body">
+                <div class="clearfix">
+                    <img class="float-left" src="{{ static_asset('assets/img/cards/paypal.png') }}" height="30">
+                    <label class="aiz-switch aiz-switch-success mb-0 float-right">
+                        <input type="checkbox" onchange="updateSettings(this, 'paypal_payment')" <?php if(\App\BusinessSetting::where('type', 'paypal_payment')->first()->value == 1) echo "checked";?>>
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+                <div class="alert text-center" style="color: #004085;background-color: #cce5ff;border-color: #b8daff;margin-bottom:0;margin-top:10px;">
+                    {{ translate('You need to configure Paypal correctly to enable this feature') }}. <a href="{{ route('payment_method.index') }}">{{ translate('Configure Now') }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Stripe Payment Activation')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <div class="clearfix">
+                    <img   class="float-left" src="{{ static_asset('assets/img/cards/stripe.png') }}" height="30">
+                    <label class="aiz-switch aiz-switch-success mb-0 float-right">
+                        <input type="checkbox" onchange="updateSettings(this, 'stripe_payment')" <?php if(\App\BusinessSetting::where('type', 'stripe_payment')->first()->value == 1) echo "checked";?>>
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+                <div class="alert" style="color: #004085;background-color: #cce5ff;border-color: #b8daff;margin-bottom:0;margin-top:10px;">
+                    You need to configure Stripe correctly to enable this feature. <a href="{{ route('payment_method.index') }}">Configure Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Cash Payment Activation')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <div class="clearfix">
+                    <img class="float-left" src="{{ static_asset('assets/img/cards/cod.png') }}" height="30">
+                    <label class="aiz-switch aiz-switch-success mb-0 float-right">
+                        <input type="checkbox" onchange="updateSettings(this, 'cash_payment')" <?php if(\App\BusinessSetting::where('type', 'cash_payment')->first()->value == 1) echo "checked";?>>
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<h4 class="text-center text-muted mt-4">{{translate('Social Media Login')}}</h4>
+<div class="row">
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Facebook login')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <label class="aiz-switch aiz-switch-success mb-0">
+                    <input type="checkbox" onchange="updateSettings(this, 'facebook_login')" <?php if(\App\BusinessSetting::where('type', 'facebook_login')->first()->value == 1) echo "checked";?>>
+                    <span class="slider round"></span>
+                </label>
+                <div class="alert" style="color: #004085;background-color: #cce5ff;border-color: #b8daff;margin-bottom:0;margin-top:10px;">
+                    {{ translate('You need to configure Facebook Client correctly to enable this feature') }}. <a href="{{ route('social_login.index') }}">{{ translate('Configure Now') }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Google login')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <label class="aiz-switch aiz-switch-success mb-0">
+                    <input type="checkbox" onchange="updateSettings(this, 'google_login')" <?php if(\App\BusinessSetting::where('type', 'google_login')->first()->value == 1) echo "checked";?>>
+                    <span class="slider round"></span>
+                </label>
+                <div class="alert" style="color: #004085;background-color: #cce5ff;border-color: #b8daff;margin-bottom:0;margin-top:10px;">
+                    {{ translate('You need to configure Google Client correctly to enable this feature') }}. <a href="{{ route('social_login.index') }}">{{ translate('Configure Now') }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="mb-0 h6 text-center">{{translate('Twitter login')}}</h3>
+            </div>
+            <div class="card-body text-center">
+                <label class="aiz-switch aiz-switch-success mb-0">
+                    <input type="checkbox" onchange="updateSettings(this, 'twitter_login')" <?php if(\App\BusinessSetting::where('type', 'twitter_login')->first()->value == 1) echo "checked";?>>
+                    <span class="slider round"></span>
+                </label>
+                <div class="alert" style="color: #004085;background-color: #cce5ff;border-color: #b8daff;margin-bottom:0;margin-top:10px;">
+                    {{ translate('You need to configure Twitter Client correctly to enable this feature') }}. <a href="{{ route('social_login.index') }}">{{ translate('Configure Now') }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+        function updateSettings(el, type){
+            if($(el).is(':checked')){
+                var value = 1;
+            }
+            else{
+                var value = 0;
+            }
+            $.post('{{ route('business_settings.update.activation') }}', {_token:'{{ csrf_token() }}', type:type, value:value}, function(data){
+                if(data == '1'){
+                    AIZ.plugins.notify('success', 'Settings updated successfully');
+                }
+                else{
+                    AIZ.plugins.notify('danger', 'Something went wrong');
+                }
+            });
+        }
+    </script>
+@endsection
