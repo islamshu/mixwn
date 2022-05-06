2a137f6260b95cb753bcdf3b0a34dc4fb67eda32
islamshu12 <islamshu12@gmail.com>
2022-05-06 16:51:03 +0300

    d

--- resources/views/frontend/order_confirmed.blade.php
+++ resources/views/frontend/order_confirmed.blade.php
@@ -18,13 +18,13 @@
                         <div class="col done">
                             <div class="text-center text-success">
                                 <i class="la-3x mb-2 las la-map"></i>
-                                <h3 class="fs-14 fw-600 d-none d-lg-block text-capitalize">{{ translate('2. Shipping info')}}</h3>
+                                <h3 class="fs-14 fw-600 d-none d-lg-block text-capitalize" style="color: black !important">{{ translate('2. Shipping info')}}</h3>
                             </div>
                         </div>
                         <div class="col done">
                             <div class="text-center text-success">
                                 <i class="la-3x mb-2 las la-truck"></i>
-                                <h3 class="fs-14 fw-600 d-none d-lg-block text-capitalize">{{ translate('3. Delivery info')}}</h3>
+                                <h3 class="fs-14 fw-600 d-none d-lg-block text-capitalize"  style="color: black !important">{{ translate('3. Delivery info')}}</h3>
                             </div>
                         </div>
                         <div class="col done">
