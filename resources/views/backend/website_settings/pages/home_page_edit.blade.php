e7947ec47f6ab776d576fda76890eef17cdf8d1c
islamshu12 <islamshu12@gmail.com>
2022-05-06 16:01:08 +0300

    d

--- resources/views/backend/website_settings/pages/home_page_edit.blade.php
+++ resources/views/backend/website_settings/pages/home_page_edit.blade.php
@@ -470,8 +470,10 @@
 						<label class="col-md-2 col-from-label">{{translate('Top Brands (Max 10)')}}</label>
 						<div class="col-md-10">
 							<input type="hidden" name="types[]" value="top10_brands">
+							{{ dd(json_decode(get_setting('top10_brands')) }}
 							<select name="top10_brands[]" class="form-control aiz-selectpicker" multiple data-max-options="10" data-live-search="true" >
 								@foreach (\App\Brand::all() as $key => $brand)
+								
 									<option value="{{ $brand->id }}" @if(in_array($brand->id, json_decode(get_setting('top10_brands')))) selected @endif>{{ $brand->getTranslation('name') }}</option>
 								@endforeach
 							</select>
