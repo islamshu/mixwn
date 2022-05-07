1e3b36b21f4c4e60fb7ace47ca7d7b3dd6204589
islamshu12 <islamshu12@gmail.com>
2022-05-07 22:39:37 +0300

    d

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -55,7 +55,7 @@
                 @php $banner_1_imags = json_decode(get_setting('home_banner1_images')); @endphp
                 @foreach ($banner_1_imags as $key => $value)
                     <div class="col-xl col-md-6 ">
-                        <div class="mb-3 mb-lg-0 borderr" style="border: 2px solid red;">
+                        <div class="mb-3 mb-lg-0 newnew" style="border: 2px solid red;">
                             <a href="{{ json_decode(get_setting('home_banner1_links'), true)[$key] }}" class="d-block text-reset">
                                 <img style="width: 100% !important;height: 200px !important;" src="{{ static_asset('assets/img/placeholder-rect.jpg') }}" data-src="{{ uploaded_asset($banner_1_imags[$key]) }}" alt="{{ env('APP_NAME') }} promo" class="img-fluid lazyload">
                             </a>
