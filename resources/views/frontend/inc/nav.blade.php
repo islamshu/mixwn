c9056c1bcccb94c08f4c82ae6454c3267821401f
islamshu12 <islamshu12@gmail.com>
2022-05-07 18:21:05 +0300

    f

--- resources/views/frontend/inc/nav.blade.php
+++ resources/views/frontend/inc/nav.blade.php
@@ -96,9 +96,9 @@
                             $header_logo = get_setting('header_logo');
                         @endphp
                         @if($header_logo != null)
-                            <img width="200" height="100" src="{{ uploaded_asset($header_logo) }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40">
+                            <img width="200" height="100" src="{{ uploaded_asset($header_logo) }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" >
                         @else
-                        <img width="200" height="100"  src="{{ static_asset('assets/img/logo.png') }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40">
+                        <img width="200" height="100"  src="{{ static_asset('assets/img/logo.png') }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" >
                         @endif
                     </a>
 
