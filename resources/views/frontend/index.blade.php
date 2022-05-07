e40b486d97dc3a00c078696f4fe79b3378875784
islamshu12 <islamshu12@gmail.com>
2022-05-07 22:08:17 +0300

    ب

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -54,8 +54,8 @@
             <div class="row gutters-10">
                 @php $banner_1_imags = json_decode(get_setting('home_banner1_images')); @endphp
                 @foreach ($banner_1_imags as $key => $value)
-                    <div class="col-xl col-md-6 borderr" style="border: 2px solid red;">
-                        <div class="mb-3 mb-lg-0">
+                    <div class="col-xl col-md-6 ">
+                        <div class="mb-3 mb-lg-0 borderr" style="border: 2px solid red;">
                             <a href="{{ json_decode(get_setting('home_banner1_links'), true)[$key] }}" class="d-block text-reset">
                                 <img style="width: 100% !important;height: 200px !important;" src="{{ static_asset('assets/img/placeholder-rect.jpg') }}" data-src="{{ uploaded_asset($banner_1_imags[$key]) }}" alt="{{ env('APP_NAME') }} promo" class="img-fluid lazyload">
                             </a>
